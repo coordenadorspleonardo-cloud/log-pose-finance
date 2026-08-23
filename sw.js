2b9bb5bf95d66c82e5a213b16c2328ab24b6810f
coordenadorspleonardo-cloud <coordenadorspleonardo@grupodiasbrasil.com.br>
2026-08-23 19:57:47 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,21 @@
-const CACHE_NAME = "log-pose-v2";
+const CACHE_NAME = "log-pose-v3";
 self.addEventListener("install", e => {
   self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(c =>
-      Promise.allSettled(["./", "./index.html", "./manifest.json"].map(url => c.add(url)))
-    )
-  );
 });
 self.addEventListener("activate", e => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
   );
   self.clients.claim();
 });
 self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
 });
